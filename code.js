6bf7dcbdafee0721342e2504b598250be8877036
Marco Antonio Soliz Lazarte <2023_marco.soliz@iticbcn.cat>
2023-11-22 19:44:19 +0100

    Añadida function setMines y esMina

--- code.js
+++ code.js
@@ -25,23 +25,19 @@ function iniciarPartida() {
     } else {
         numColumnes = pedirNumColumnes;
     }
-
-    console.log(numFiles);
-    console.log(numColumnes);
     crearTaulell();
+    setMines();
 }
 
 function crearTaulell() {
-    let tablaHTML = "<table class='table' styles='border:1px solid black'>";
+    let tablaHTML = "<table class='table' style='border: 2px solid black; border-collapse: collapse; padding:2px;'>";
+
 
     for (let i = 0; i < numFiles; i++) {
         tablaHTML += "<tr>";
-        tablaHTML += `<th><img src='${fons}'></th>`;
-
         for (let x = 0; x < numColumnes; x++) {
-            tablaHTML += `<td><img src='${fons}'></td>`;
+            tablaHTML += `<td id="${i},${x}" data-mina="false" style='border: 2px solid black; border-collapse: collapse; padding:2px;'><img onclick="obreCasella(${x},${i})"src='${fons}'></td>`;
         }
-
         tablaHTML += "</tr>";
     }
 
@@ -49,6 +45,44 @@ function crearTaulell() {
     document.getElementById("taulell").innerHTML = tablaHTML;
 }
 
-function obreCasella(){
-    
+function obreCasella(i, x) {
+    alert(`fila ${i} columna ${x}`);
+    if (condition) {
+
+    } else {
+        
+
+    }
+
+}
+
+function setMines() {
+    let cassillasTotales = numFiles * numColumnes;
+    console.log(cassillasTotales);
+    let numMinas = Math.floor(0.17 * cassillasTotales);
+    for (let index = 0; index < numMinas; index++) {
+        let fila = Math.floor(Math.random() * numFiles);
+        let columna = Math.floor(Math.random() * numColumnes);
+        let td = document.getElementById(`${fila},${columna}`);
+        console.log(td);
+        if (td.dataset.mina ==="true") {
+            index--;
+        } else {
+            td.dataset.mina = "true";
+        }
+
+    }
+
+}
+function calculaAdjacents() {
+
+}
+function esMina(i, x) {
+    let td = document.getElementById(`${index},${j}`);
+    if (td.dataset.mina == "true") {
+        return true;
+    } else {
+        return false;
+    }
+
 }
