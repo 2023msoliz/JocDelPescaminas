cbe223d45d5b09119c030fd73170b00ff148b22c
2023msoliz <2023_marco.soliz@iticbcn.cat>
2023-11-30 22:50:39 +0100

    Añadido funcion deshabilitarClicEnCeldas y contarMinasAlrededor

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # Pescaminas
+
+Este es un juego de Buscaminas simple implementado en JavaScript, HTML y CSS.
+
+## Cómo Jugar
+
+1. Haz clic en el botón "Iniciar Partida" para comenzar una nueva partida.
+2. Selecciona el número de filas y columnas para personalizar el tamaño del tablero (entre 10 y 30).
+3. Haz clic en las celdas para abrirlas y descubre si hay minas.
+4. Si encuentras una mina, el juego termina. De lo contrario, se mostrará el número de minas adyacentes en las celdas abiertas.
+
+## Reglas del Juego
+
+- El juego termina si se hace clic en una mina.
+- El objetivo es abrir todas las celdas sin encontrar una mina.
+- Cada celda abierta muestra el número de minas adyacentes.
+
+## Personalizaciones
+
+Puedes personalizar la apariencia del juego ajustando los estilos en el archivo `code.js`. También puedes agregar nuevas funcionalidades según sea necesario.
+
+## Contribuciones
+
+¡Contribuciones son bienvenidas! Si encuentras errores o tienes mejoras, no dudes en crear un problema o enviar una solicitud de extracción.
+
+## Autor
+
+[Nombre del Autor]
+
+## Licencia
+
+Este proyecto está bajo la Licencia MIT. Consulta el archivo `LICENSE` para obtener más detalles.
--- code.js
+++ code.js
@@ -1,13 +1,18 @@
 let numFiles = 0;
 let numColumnes = 0;
+let juegoTerminado = false;
 
+// Rutas de las imágenes utilizadas
 const badera = "imagenes/badera20px.jpg";
 const fons = "imagenes/fons20px.jpg";
 const mina = "imagenes/mina20px.jpg";
 
+// Función para iniciar una nueva partida
 function iniciarPartida() {
+    // Solicitar al usuario el número de filas
     let pedirNumFiles = parseInt(prompt("Introduce el número de filas."));
 
+    // Validar y ajustar el número de filas dentro del rango permitido (10-30)
     if (pedirNumFiles > 30) {
         numFiles = 30;
     } else if (pedirNumFiles < 10) {
@@ -16,8 +21,10 @@ function iniciarPartida() {
         numFiles = pedirNumFiles;
     }
 
+    // Solicitar al usuario el número de columnas
     let pedirNumColumnes = parseInt(prompt("Introduce el número de columnas"));
 
+    // Validar y ajustar el número de columnas dentro del rango permitido (10-30)
     if (pedirNumColumnes > 30) {
         numColumnes = 30;
     } else if (pedirNumColumnes < 10) {
@@ -25,18 +32,20 @@ function iniciarPartida() {
     } else {
         numColumnes = pedirNumColumnes;
     }
+
+    // Crear el tablero y colocar las minas
     crearTaulell();
     setMines();
 }
 
+// Función para crear el tablero HTML
 function crearTaulell() {
-    let tablaHTML = "<table class='table' style='border: 2px solid black; border-collapse: collapse; padding:2px;'>";
-
+    let tablaHTML = "<table class='table' style='border: 2px solid black; border-collapse: collapse; padding:2px; margin: 0 auto;'>";
 
     for (let i = 0; i < numFiles; i++) {
         tablaHTML += "<tr>";
         for (let x = 0; x < numColumnes; x++) {
-            tablaHTML += `<td id="${i},${x}" data-mina="false" style='border: 2px solid black; border-collapse: collapse; padding:2px;'><img onclick="obreCasella(${x},${i})"src='${fons}'></td>`;
+            tablaHTML += `<td id="${i},${x}" data-mina="false" data-num-mines="0" style='border: 2px solid black; border-collapse: collapse; padding:2px;'><img onclick="obreCasella(${x},${i})" src='${fons}'></td>`;
         }
         tablaHTML += "</tr>";
     }
@@ -45,44 +54,103 @@ function crearTaulell() {
     document.getElementById("taulell").innerHTML = tablaHTML;
 }
 
+
+// Función para manejar el evento de abrir una casilla
 function obreCasella(i, x) {
-    alert(`fila ${i} columna ${x}`);
-    if (condition) {
-
-    } else {
-        
-
+    if (juegoTerminado) {
+        return; // Evitar acciones adicionales si el juego ya ha terminado
     }
 
+    let td = document.getElementById(`${x},${i}`);
+    if (esMina(x, i)) {
+        // Muestra todas las minas
+        mostrarMinas();
+        alert("¡Boom! Has encontrado una mina. Juego terminado.");
+
+        // Desactivar el juego
+        juegoTerminado = true;
+
+        // Deshabilitar el evento de clic en todas las celdas
+        deshabilitarClicEnCeldas();
+        document.getElementById("iniciarBtn").disabled = false;
+    } else {
+        let numMinasAlrededor = contarMinasAlrededor(x, i);
+        td.innerHTML = `<div style="display: flex; align-items: center; justify-content: center; height: 100%;">${numMinasAlrededor}</div>`;
+    }
 }
 
+// Función que muestra todas las minas al finalizar el juego
+function mostrarMinas() {
+    let celdas = document.querySelectorAll('td');
+    celdas.forEach((celda) => {
+        let coordenadas = celda.id.split(",");
+        let x = parseInt(coordenadas[1]);
+        let y = parseInt(coordenadas[0]);
+
+        if (esMina(x, y)) {
+            celda.innerHTML = `<img src='${mina}' alt='mina'>`;
+        }
+    });
+}
+
+// Función que deshabilita el evento de clic en todas las celdas
+function deshabilitarClicEnCeldas() {
+    let celdas = document.querySelectorAll('td');
+    celdas.forEach((celda) => {
+        celda.onclick = null; // Deshabilitar el evento de clic
+    });
+}
+
+// Función para contar el número de minas alrededor de una celda
+function contarMinasAlrededor(x, y) {
+    let contador = 0;
+    for (let i = x - 1; i <= x + 1; i++) {
+        for (let j = y - 1; j <= y + 1; j++) {
+            if (i >= 0 && i < numColumnes && j >= 0 && j < numFiles && !(i === x && j === y)) {
+                if (esMina(i, j)) {
+                    contador++;
+                }
+            }
+        }
+    }
+    return contador;
+}
+
+// Función para colocar las minas en posiciones aleatorias
 function setMines() {
     let cassillasTotales = numFiles * numColumnes;
-    console.log(cassillasTotales);
     let numMinas = Math.floor(0.17 * cassillasTotales);
+
     for (let index = 0; index < numMinas; index++) {
-        let fila = Math.floor(Math.random() * numFiles);
-        let columna = Math.floor(Math.random() * numColumnes);
-        let td = document.getElementById(`${fila},${columna}`);
-        console.log(td);
-        if (td.dataset.mina ==="true") {
-            index--;
-        } else {
-            td.dataset.mina = "true";
+        let fila, columna, td;
+        do {
+            fila = Math.floor(Math.random() * numFiles);
+            columna = Math.floor(Math.random() * numColumnes);
+            td = document.getElementById(`${fila},${columna}`);
+        } while (td.dataset.mina === "true");
+
+        td.dataset.mina = "true";
+        incrementaNumMinasAdyacentes(fila, columna);
+    }
+}
+
+// Función para incrementar el contador de minas adyacentes en las celdas vecinas
+function incrementaNumMinasAdyacentes(fila, columna) {
+    for (let i = fila - 1; i <= fila + 1; i++) {
+        for (let j = columna - 1; j <= columna + 1; j++) {
+            if (i >= 0 && i < numFiles && j >= 0 && j < numColumnes && !(i === fila && j === columna)) {
+                let td = document.getElementById(`${i},${j}`);
+                td.dataset.numMines = parseInt(td.dataset.numMines) + 1;
+            }
         }
-
     }
-
 }
+
+// Función para verificar si una celda contiene una mina
+function esMina(x, y) {
+    let td = document.getElementById(`${y},${x}`);
+    return td.dataset.mina === "true";
+}
+
 function calculaAdjacents() {
-
-}
-function esMina(i, x) {
-    let td = document.getElementById(`${index},${j}`);
-    if (td.dataset.mina == "true") {
-        return true;
-    } else {
-        return false;
-    }
-
 }
